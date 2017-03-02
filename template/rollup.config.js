a2a0c41c0d3daa23907af0808f25c377a15952c0
limichange <limichange@hotmail.com>
2017-03-02 16:59:04 +0800

    🎨  - Improving structure / format of the code.

--- template/rollup.config.js
+++ template/rollup.config.js
@@ -2,11 +2,13 @@ import buble from 'rollup-plugin-buble'
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 
-let targets = [ { dest: 'dist/{{ name }}.js', format: 'umd' } ]
+let targets = [
+  { dest: 'dist/{{ name }}.common.js', format: 'cjs' }
+]
 
 if (process.env.BUILD) {
   targets = targets.concat([
-    { dest: 'dist/{{ name }}.common.js', format: 'cjs' },
+    { dest: 'dist/{{ name }}.js', format: 'umd' },
     { dest: 'dist/{{ name }}.es5.js', format: 'es' }
   ])
 }
